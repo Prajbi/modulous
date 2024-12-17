0cd99b31c3681e98b91c5622594b81dae4d92ec4
Prajwal Biradar <112881872+Prajbi@users.noreply.github.com>
2024-12-17 16:58:13 +0530

    Add files via upload

---
+++ modulo.java
@@ -0,0 +1,15 @@
+//import java.util.*;
+//import java.io.*;
+
+public class modulo {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        int a = 13;
+        int b = 5;
+        int k = a % b;
+        System.out.println("remainder= " + k);
+    }
+
+}
